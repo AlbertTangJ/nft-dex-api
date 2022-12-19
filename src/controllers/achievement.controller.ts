1ffe83eade788b3e78ee4f86438688502fff1fab
webmaster@tribe3.xyz <webmaster@tribe3.xyz>
2022-12-19 12:29:05 +0800

    Add showCompleted param to task list api

--- src/controllers/achievement.controller.ts
+++ src/controllers/achievement.controller.ts
@@ -1,13 +1,4 @@
-import {
-  JsonController,
-  Get,
-  QueryParam,
-  Post,
-  BodyParam,
-  Authorized,
-  Param,
-  Body,
-} from "routing-controllers";
+import { JsonController, Get, QueryParam } from "routing-controllers";
 import { AchievementService, UserService } from "../services";
 import { Service } from "typedi";
 import { ApiResponse, ResponseStatus } from "src/helpers/apiResponse";
@@ -16,43 +7,38 @@ import { RepeatPeriod } from "@prisma/client";
 @JsonController()
 @Service()
 export class AchievementController {
-  constructor(
-    private userService: UserService,
-    private achievementService: AchievementService
-  ) {}
+  constructor(private userService: UserService, private achievementService: AchievementService) {}
 
   @Get("/achievement/referral/list")
   async referralAchievements(@QueryParam("userAddress") userAddress: string) {
-    let result = await this.achievementService.getReferralAchievements(
-      userAddress,
-      100
-    );
+    let result = await this.achievementService.getReferralAchievements(userAddress, 100);
 
-    let responseData = result.map((item) => {
+    let responseData = result.map(item => {
       let obj: any = {
         userId:
-          item.referralUser?.userInfo?.username?.length > 0
-            ? item.referralUser.userInfo.username
-            : item.referralUser.userInfo.userAddress,
+          item.referralUser?.userInfo?.username?.length > 0 ? item.referralUser.userInfo.username : item.referralUser.userInfo.userAddress,
         pointGained: item.pointEarned,
         status: item.achievement.title,
-        createTime: item.createTime,
+        createTime: item.createTime
       };
       return obj;
     });
 
-    return new ApiResponse(ResponseStatus.Success)
-      .setData(responseData)
-      .toObject();
+    return new ApiResponse(ResponseStatus.Success).setData(responseData).toObject();
   }
 
   @Get("/achievement/task/list")
-  async taskList(@QueryParam("userAddress", { required: true }) userAddress: string) {
+  async taskList(
+    @QueryParam("userAddress", { required: true }) userAddress: string,
+    @QueryParam("showCompleted") showCompleted: boolean = true
+  ) {
     let weeklyTasks = [];
     let promotionTasks = [];
     let oneTimeTasks = [];
     let achievementList = await this.achievementService.getUserAchievementList(userAddress);
     for (let achievement of achievementList) {
+      if (!showCompleted && achievement.step === achievement.progress) continue;
+
       if (achievement.code.startsWith("E")) {
         promotionTasks.push(achievement);
       } else if (achievement.repeatPeriod === RepeatPeriod.Weekly) {
