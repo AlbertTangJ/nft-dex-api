2b43585d332701ef7019367a4abbc8b4715c14c7
webmaster-tribe3 <webmaster@tribe3.xyz>
2023-04-13 19:38:42 +0800

    update infura client

--- src/helpers/infuraClient.ts
+++ src/helpers/infuraClient.ts
@@ -3,9 +3,9 @@ import { ethers } from "ethers";
 let infuraProvider: ethers.providers.JsonRpcProvider;
 
 if (process.env.NODE_ENV === "production") {
-  infuraProvider = new ethers.providers.JsonRpcProvider("https://arbitrum-goerli.infura.io/v3/6ec994a8abd8472bb95b74f1bea73875");
+  infuraProvider = new ethers.providers.JsonRpcProvider("https://arbitrum-mainnet.infura.io/v3/2f7efd3a292149809ca5c503ee56d6f2");
 } else {
-  infuraProvider = new ethers.providers.JsonRpcProvider("https://arbitrum-goerli.infura.io/v3/6ec994a8abd8472bb95b74f1bea73875");
+  infuraProvider = new ethers.providers.JsonRpcProvider("https://arbitrum-mainnet.infura.io/v3/2f7efd3a292149809ca5c503ee56d6f2");
 }
 
 export default infuraProvider;
