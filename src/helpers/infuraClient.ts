8b69e430367503d2ea20971087f3edf93d77ad5c
AlbertTangJ <albert@tribe3.xyz>
2023-06-08 17:12:34 +0800

    change to rpc json provider

--- src/helpers/infuraClient.ts
+++ src/helpers/infuraClient.ts
@@ -4,13 +4,11 @@ let infuraProvider: ethers.providers.JsonRpcProvider;
 
 if (process.env.NODE_ENV === "production") {
   infuraProvider = new ethers.providers.InfuraProvider(
-    "arbitrum-goerli",
-    "6ec994a8abd8472bb95b74f1bea73875"
+    "https://arbitrum-goerli.infura.io/v3/6ec994a8abd8472bb95b74f1bea73875"
   );
 } else {
-  infuraProvider = new ethers.providers.InfuraProvider(
-    "arbitrum-goerli",
-    "bc7ea61a955c4f07b1dc8ad4dc08ba08"
+  infuraProvider = new ethers.providers.JsonRpcProvider(
+    "https://arbitrum-goerli.infura.io/v3/bc7ea61a955c4f07b1dc8ad4dc08ba08"
   );
 }
 
