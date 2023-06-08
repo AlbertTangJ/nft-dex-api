84a4b51c8da4e809f3e0bacfac3738f84cf69a45
AlbertTangJ <albert@tribe3.xyz>
2023-06-08 18:29:58 +0800

    update infura to alchemy url provider

--- src/helpers/infuraClient.ts
+++ src/helpers/infuraClient.ts
@@ -1,15 +1,12 @@
 import { ethers } from "ethers";
 
 let infuraProvider: ethers.providers.JsonRpcProvider;
-
 if (process.env.NODE_ENV === "production") {
-  infuraProvider = new ethers.providers.InfuraProvider(
-    "https://arbitrum-goerli.infura.io/v3/6ec994a8abd8472bb95b74f1bea73875"
-  );
+  infuraProvider = new ethers.providers.JsonRpcProvider(
+    "https://arb-goerli.g.alchemy.com/v2/I8yTFUYDb_bso5E9di3Kn_pu4UGzywGr");
 } else {
   infuraProvider = new ethers.providers.JsonRpcProvider(
-    "https://arbitrum-goerli.infura.io/v3/bc7ea61a955c4f07b1dc8ad4dc08ba08"
-  );
+    "https://arb-goerli.g.alchemy.com/v2/I8yTFUYDb_bso5E9di3Kn_pu4UGzywGr");
 }
 
 export default infuraProvider;
